95abcafe601e088805380985e4244f8ce842b8d1
ZaneShan <dchen113@ucsc.edu>
2024-10-15 14:34:43 -0700

    step 1

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement('h1');
+title.textContent = 'title';
+document.body.append(title);
+
+const canvas = document.createElement('canvas');
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "mainCanvas"
+document.body.append(canvas);
