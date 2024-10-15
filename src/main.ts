b14761c0505ffb0fa1b9f91b42112cc469e3c76a
ZaneShan <dchen113@ucsc.edu>
2024-10-15 14:53:57 -0700

    step 2

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,41 @@ const title = document.createElement('h1');
 title.textContent = 'title';
 document.body.append(title);
 
+// CANVAS //
 const canvas = document.createElement('canvas');
 canvas.width = 256;
 canvas.height = 256;
 canvas.id = "mainCanvas"
 document.body.append(canvas);
+
+// drawing styles
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = 'black'; // Line color
+ctx.lineWidth = 2; // Line width
+
+let isDrawing = false; // check mousedown variable
+// start
+canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+    ctx.moveTo(event.offsetX, event.offsetY);
+});
+// draw
+canvas.addEventListener('mousemove', (event) => {
+    if (!isDrawing) return;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+});
+// stop
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+    ctx.beginPath(); // start a new path for next stroke
+});
+
+const clearButton = document.createElement('button');
+clearButton.textContent = 'Clear';
+document.body.append(clearButton);
+
+// Clear the canvas when the button is clicked
+clearButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
